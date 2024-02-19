a5a91e505bc632d65d27b566db19e3fd4935b87f
Pvngu <omargt151102@gmail.com>
2024-02-18 17:40:36 -0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pvngu.github.io/Portfolio',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://pvngu.github.io/Portfolio'
 });
