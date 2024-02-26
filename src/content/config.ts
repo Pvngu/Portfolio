a2035296728b3fccec825b3d3ef1eb43b9f371e1
Pvngu <omargt151102@gmail.com>
2024-02-26 13:17:51 -0800

    update project page design and add img to andrea

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const projectsCollection = defineCollection({
     schema: ({image}) => z.object({
         title: z.string(),
         description: z.string(),
+        date: z.string(),
         image: image(),
         tags: z.array(z.string()),
         tech: z.array(z.string()),
